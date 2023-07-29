1461a0dc8aa340f86d91634e3a0c0e8659eb29ae
Abdelrahman Abo Al Nasr <59171143+abdelrahmanTD@users.noreply.github.com>
2023-07-29 19:58:55 +0300

    Initial Commit

---
+++ RadioButton-SwiftUI/RadioButton_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioButton_SwiftUIApp.swift
+//  RadioButton-SwiftUI
+//
+//  Created by Abdelrahman Abo Al Nasr on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RadioButton_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
